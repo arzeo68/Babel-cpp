730341cfabe07c51d6fb294afea600ab24759430
Jean-Gaël Choppe <whosdad@desktop-l70kbgl.home>
2020-09-27 00:23:11 +0200

    feat(feat-router): add addRoute and handler method to the Router

--- server/src/Router/Router.cpp
+++ server/src/Router/Router.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Router.hpp"
+
+Server::API::Router::Router() {
+
+}
+
+bool Server::API::Router::addRoute(std::shared_ptr<Route> route) {
+    _Routes[route.getName()] = route;
+    return true;
+}
+
+bool Server::API::Router::handler(std::string routePath) {
+    if (_Routes.find(routePath) != _Routes.end()) {
+        _Routes[routePath]->handler();
+    } else {
+        return true;
+    }
+    return false;
+}
