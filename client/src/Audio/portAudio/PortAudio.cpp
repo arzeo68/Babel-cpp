45cec887bfe7714ce33820376c8bc71b4cc4c60c
alexis WALTER <alexis.walter@epitech.eu>
2020-09-28 17:09:41 +0200

    [+] port audio

--- client/src/Audio/portAudio/PortAudio.cpp
+++ client/src/Audio/portAudio/PortAudio.cpp
@@ -2,308 +2,134 @@
 // Created by alexis on 20/09/2020.
 //
 
-#include "PortAudio.hpp"
 #include <iostream>
+#include <cstring>
+#include "PortAudio.hpp"
 
 PortAudio::PortAudio()
 {
-    _err = Pa_Initialize();
-    std::cout << "ntm" << std::endl;
+    _inCall = false;
+    _recording = false;
+    _listening = false;
+    // Init
+    if (Pa_Initialize() != paNoError) {
+        std::cout << "Can't Pa_Initialize" << std::endl;
+        return;
+    }
+    // Input
+    inputParams.device = Pa_GetDefaultInputDevice();
+    if (inputParams.device == paNoDevice) {
+        std::cout << "Can't Pa_GetDefaultInputDevice" << std::endl;
+        return;
+    }
+    inputParams.channelCount = 2;
+    inputParams.sampleFormat = paFloat32;
+    inputParams.suggestedLatency = Pa_GetDeviceInfo(inputParams.device)->defaultLowInputLatency;
+    inputParams.hostApiSpecificStreamInfo = NULL;
+    // Output
+    outputParams.device = Pa_GetDefaultOutputDevice();
+    if (outputParams.device == paNoDevice) {
+        std::cout << "Can't Pa_GetDefaultOutputDevice" << std::endl;
+        return;
+    }
+    outputParams.channelCount = 2;
+    outputParams.sampleFormat = paFloat32;
+    outputParams.suggestedLatency = Pa_GetDeviceInfo(outputParams.device)->defaultLowOutputLatency;
+    outputParams.hostApiSpecificStreamInfo = NULL;
+    _init = true;
+    std::cout << "Port audio initialized." << std::endl;
 }
 
-
-/// TEST PORT AUDIO
-
-#include <stdio.h>
-#include <stdlib.h>
-#include "portaudio.h"
-
-/* #define SAMPLE_RATE  (17932) // Test failure to open with this value. */
-#define SAMPLE_RATE  (44100)
-#define FRAMES_PER_BUFFER (512)
-#define NUM_SECONDS     (5)
-#define NUM_CHANNELS    (2)
-/* #define DITHER_FLAG     (paDitherOff) */
-#define DITHER_FLAG     (0) /**/
-/** Set to 1 if you want to capture the recording to a file. */
-#define WRITE_TO_FILE   (0)
-
-/* Select sample format. */
-#if 1
-    #define PA_SAMPLE_TYPE  paFloat32
-typedef float SAMPLE;
-    #define SAMPLE_SILENCE  (0.0f)
-    #define PRINTF_S_FORMAT "%.8f"
-#elif 1
-#define PA_SAMPLE_TYPE  paInt16
-typedef short SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#define PRINTF_S_FORMAT "%d"
-#elif 0
-#define PA_SAMPLE_TYPE  paInt8
-typedef char SAMPLE;
-#define SAMPLE_SILENCE  (0)
-#define PRINTF_S_FORMAT "%d"
-#else
-#define PA_SAMPLE_TYPE  paUInt8
-typedef unsigned char SAMPLE;
-#define SAMPLE_SILENCE  (128)
-#define PRINTF_S_FORMAT "%d"
-#endif
-
-typedef struct
+void PortAudio::startStream()
 {
-    int          frameIndex;  /* Index into sample array. */
-    int          maxFrameIndex;
-    SAMPLE      *recordedSamples;
+    if (_recording || !_init)
+        return;
+    if (Pa_OpenStream(&stat, &inputParams, NULL, 48000, 480,
+        paClipOff, PortAudio::recordCallBack, this) != paNoError) {
+        std::cout << "Can't Pa_GetDefaultInputDevice" << std::endl;
+        return;
+    }
+    if (Pa_StartStream(stat) != paNoError) {
+        std::cout << "Can't Pa_StartStream" << std::endl;
+        return;
+    }
+    _recording = true;
 }
-    paTestData;
 
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may be called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
-static int recordCallback( const void *inputBuffer, void *outputBuffer,
-    unsigned long framesPerBuffer,
-    const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags,
-    void *userData )
+void PortAudio::stopStream()
 {
-    paTestData *data = (paTestData*)userData;
-    const SAMPLE *rptr = (const SAMPLE*)inputBuffer;
-    SAMPLE *wptr = &data->recordedSamples[data->frameIndex * NUM_CHANNELS];
-    long framesToCalc;
-    long i;
-    int finished;
-    unsigned long framesLeft = data->maxFrameIndex - data->frameIndex;
-
-    (void) outputBuffer; /* Prevent unused variable warnings. */
-    (void) timeInfo;
-    (void) statusFlags;
-    (void) userData;
-
-    if( framesLeft < framesPerBuffer )
-    {
-        framesToCalc = framesLeft;
-        finished = paComplete;
+    if (!_recording || !_init)
+        return;
+    if (Pa_CloseStream(stat) != paNoError) {
+        std::cout << "Can't Pa_CloseStream" << std::endl;
+        return;
     }
-    else
-    {
-        framesToCalc = framesPerBuffer;
-        finished = paContinue;
-    }
-
-    if( inputBuffer == NULL )
-    {
-        for( i=0; i<framesToCalc; i++ )
-        {
-            *wptr++ = SAMPLE_SILENCE;  /* left */
-            if( NUM_CHANNELS == 2 ) *wptr++ = SAMPLE_SILENCE;  /* right */
-        }
-    }
-    else
-    {
-        for( i=0; i<framesToCalc; i++ )
-        {
-            *wptr++ = *rptr++;  /* left */
-            if( NUM_CHANNELS == 2 ) *wptr++ = *rptr++;  /* right */
-        }
-    }
-    data->frameIndex += framesToCalc;
-    return finished;
+    _recording = false;
 }
 
-/* This routine will be called by the PortAudio engine when audio is needed.
-** It may be called at interrupt level on some machines so don't do anything
-** that could mess up the system like calling malloc() or free().
-*/
-static int playCallback( const void *inputBuffer, void *outputBuffer,
-    unsigned long framesPerBuffer,
-    const PaStreamCallbackTimeInfo* timeInfo,
-    PaStreamCallbackFlags statusFlags,
-    void *userData )
+void PortAudio::startListening()
 {
-    paTestData *data = (paTestData*)userData;
-    SAMPLE *rptr = &data->recordedSamples[data->frameIndex * NUM_CHANNELS];
-    SAMPLE *wptr = (SAMPLE*)outputBuffer;
-    unsigned int i;
-    int finished;
-    unsigned int framesLeft = data->maxFrameIndex - data->frameIndex;
-
-    (void) inputBuffer; /* Prevent unused variable warnings. */
-    (void) timeInfo;
-    (void) statusFlags;
-    (void) userData;
-
-    if( framesLeft < framesPerBuffer )
-    {
-        /* final buffer... */
-        for( i=0; i<framesLeft; i++ )
-        {
-            *wptr++ = *rptr++;  /* left */
-            if( NUM_CHANNELS == 2 ) *wptr++ = *rptr++;  /* right */
-        }
-        for( ; i<framesPerBuffer; i++ )
-        {
-            *wptr++ = 0;  /* left */
-            if( NUM_CHANNELS == 2 ) *wptr++ = 0;  /* right */
-        }
-        data->frameIndex += framesLeft;
-        finished = paComplete;
+    if (_listening || !_init)
+        return;
+    if (Pa_OpenStream(&stout, NULL, &outputParams, 48000, 480, paClipOff, PortAudio::playCallBack, this) != paNoError) {
+        std::cout << "Can't Pa_OpenStream" << std::endl;
+        return;
     }
-    else
-    {
-        for( i=0; i<framesPerBuffer; i++ )
-        {
-            *wptr++ = *rptr++;  /* left */
-            if( NUM_CHANNELS == 2 ) *wptr++ = *rptr++;  /* right */
-        }
-        data->frameIndex += framesPerBuffer;
-        finished = paContinue;
+    if (Pa_StartStream(stout) != paNoError) {
+        std::cout << "Can't Pa_StartStream" << std::endl;
+        return;
     }
-    return finished;
+    _listening = true;
 }
 
-/*******************************************************************/
-int portAudioTest(void)
+void PortAudio::stopListening()
 {
-    PaStreamParameters  inputParameters,
-        outputParameters;
-    PaStream*           stream;
-    PaError             err = paNoError;
-    paTestData          data;
-    int                 i;
-    int                 totalFrames;
-    int                 numSamples;
-    int                 numBytes;
-    SAMPLE              max, val;
-    double              average;
-
-    printf("patest_record.c\n"); fflush(stdout);
-
-    data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
-    data.frameIndex = 0;
-    numSamples = totalFrames * NUM_CHANNELS;
-    numBytes = numSamples * sizeof(SAMPLE);
-    data.recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
-    if( data.recordedSamples == NULL )
-    {
-        printf("Could not allocate record array.\n");
-        goto done;
+    if (!_listening || !_init)
+        return;
+    if (Pa_CloseStream(stat) != paNoError) {
+        std::cout << "Can't Pa_CloseStream" << std::endl;
+        return;
     }
-    for( i=0; i<numSamples; i++ ) data.recordedSamples[i] = 0;
-
-    err = Pa_Initialize();
-    if( err != paNoError ) goto done;
-
-    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
-    if (inputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default input device.\n");
-        goto done;
-    }
-    inputParameters.channelCount = 2;                    /* stereo input */
-    inputParameters.sampleFormat = PA_SAMPLE_TYPE;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
-    inputParameters.hostApiSpecificStreamInfo = NULL;
-
-    /* Record some audio. -------------------------------------------- */
-    err = Pa_OpenStream(
-        &stream,
-        &inputParameters,
-        NULL,                  /* &outputParameters, */
-        SAMPLE_RATE,
-        FRAMES_PER_BUFFER,
-        paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-        recordCallback,
-        &data );
-    if( err != paNoError ) goto done;
-
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) goto done;
-    printf("\n=== Now recording!! Please speak into the microphone. ===\n"); fflush(stdout);
-
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000);
-        printf("index = %d\n", data.frameIndex ); fflush(stdout);
-    }
-    if( err < 0 ) goto done;
-
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto done;
-
-    /* Measure maximum peak amplitude. */
-    max = 0;
-    average = 0.0;
-    for( i=0; i<numSamples; i++ )
-    {
-        val = data.recordedSamples[i];
-        if( val < 0 ) val = -val; /* ABS */
-        if( val > max )
-        {
-            max = val;
-        }
-        average += val;
-    }
-
-    average = average / (double)numSamples;
-
-    printf("sample max amplitude = "PRINTF_S_FORMAT"\n", max );
-    printf("sample average = %lf\n", average );
-
-
-    /* Playback recorded data.  -------------------------------------------- */
-    data.frameIndex = 0;
-
-    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
-    if (outputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default output device.\n");
-        goto done;
-    }
-    outputParameters.channelCount = 2;                     /* stereo output */
-    outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = NULL;
-
-    printf("\n=== Now playing back. ===\n"); fflush(stdout);
-    err = Pa_OpenStream(
-        &stream,
-        NULL, /* no input */
-        &outputParameters,
-        SAMPLE_RATE,
-        FRAMES_PER_BUFFER,
-        paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-        playCallback,
-        &data );
-    if( err != paNoError ) goto done;
-
-    if( stream )
-    {
-        err = Pa_StartStream( stream );
-        if( err != paNoError ) goto done;
-
-        printf("Waiting for playback to finish.\n"); fflush(stdout);
-
-        while( ( err = Pa_IsStreamActive( stream ) ) == 1 ) Pa_Sleep(100);
-        if( err < 0 ) goto done;
-
-        err = Pa_CloseStream( stream );
-        if( err != paNoError ) goto done;
-
-        printf("Done.\n"); fflush(stdout);
-    }
-
-    done:
-    Pa_Terminate();
-    if( data.recordedSamples )       /* Sure it is NULL or valid. */
-        free( data.recordedSamples );
-    if( err != paNoError )
-    {
-        fprintf( stderr, "An error occured while using the portaudio stream\n" );
-        fprintf( stderr, "Error number: %d\n", err );
-        fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
-        err = 1;          /* Always return 0 or 1, but no other return codes. */
-    }
-    return err;
+    _listening = false;
 }
 
+void *PortAudio::getStat() const
+{
+    return stat;
+}
+
+void *PortAudio::getStout() const
+{
+    return stout;
+}
+
+int PortAudio::recordCallBack(const void *tmp_buff, void *, unsigned long frm, const PaStreamCallbackTimeInfo *, PaStreamCallbackFlags, void *obj)
+{
+    PortAudio *p = reinterpret_cast<PortAudio *>(obj);
+    float const *rptr = (float const *)tmp_buff;
+    std::vector<float> wptr;
+    wptr.reserve(frm * 2);
+    for (int i = 0; i < 480; i++)
+    {
+        wptr.emplace_back(*rptr++);
+        if constexpr (NUM_CHANNEL == 2)
+            wptr.emplace_back(*rptr++);
+    }
+    p->_m.lock();
+    p->_bufferQueue.push(std::move(wptr));
+    p->_m.unlock();
+    return 0;
+}
+
+int PortAudio::playCallBack(const void *, void *tmp_buff, unsigned long frm, const PaStreamCallbackTimeInfo *, PaStreamCallbackFlags, void *obj)
+{
+    PortAudio *p = reinterpret_cast<PortAudio *>(obj);
+    p->_m.lock();
+    if (!p->_bufferQueue.empty())
+    {
+        memcpy(tmp_buff, p->_bufferQueue.front().data(), frm * 2);
+        p->_bufferQueue.pop();
+    }
+    p->_m.unlock();
+    return 0;
+}
