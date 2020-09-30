b4b1948dbb2e2a9dce89717028792d71d61588df
Jean-Gaël Choppe <whosdad@localhost.localdomain>
2020-09-30 15:25:16 +0200

    feat(router): rm some http codes which are going to be useless

--- server/src/API/Route/HTTPCodes.hpp
+++ server/src/API/Route/HTTPCodes.hpp
@@ -20,12 +20,7 @@ namespace Server::HTTPCodes {
         FORBIDDEN = 403,
         DATA_NOT_FOUND = 404,
         METHOD_NOT_ALLOWED = 405,
-        UNSUPPORTED_MEDIA_TYPE = 415,
-        RATE_LIMIT_EXCEEDED = 429,
         INTERNAL_SERVER_ERROR = 500,
-        BAD_GATEWAY = 502,
-        SERVICE_UNAVAILABLE = 503,
-        GATEWAY_TIMEOUT = 504,
     };
 }
 
