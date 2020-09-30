4bd149b3a9f06f416bbdc7664342433b21c9a3ff
Jean-Gaël Choppe <whosdad@localhost.localdomain>
2020-09-30 14:01:56 +0200

    feat(router): rm useless files, fix conflicts & resolve review comments

--- server/src/API/Route/Response.hpp
+++ server/src/API/Route/Response.hpp
@@ -8,21 +8,12 @@
 #include "HTTPCodes.hpp"
 #include <string>
 
-<<<<<<< HEAD:server/src/API/Route/Response.hpp
 namespace Server {
-        struct Response {
-            HTTPCodes::HTTPCodes_t code;
-            std::string msg;
-        };
-    }
-=======
-namespace Server::API {
-    struct Error {
-        HTTPCodes::HTTPCodes_t code;
+    struct Response {
+        HTTPCodes::HTTPCodes_e code;
         std::string msg;
     };
 }
->>>>>>> c3a1b742525581a1d73001aacdf11a924600b210:server/src/API/Route/Error.hpp
 
 
 #endif //BABEL_RESPONSE_HPP
