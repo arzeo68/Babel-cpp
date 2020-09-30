dc6c88f65575afece1121002125f99ddb1f6ec28
Jean-Gaël Choppe <whosdad@localhost.localdomain>
2020-09-30 15:42:52 +0200

    feat(user): add routes functions

--- server/src/User/User.hpp
+++ server/src/User/User.hpp
@@ -9,5 +9,20 @@ class User {
     public:
     User() = default;
     ~User() = default;
+
+    static bool Login();
+    static bool Register();
+    static bool UserExist();
+    static bool Status();
+    static bool Friend();
+
+    static bool PutFriend();
+    static bool PostFriend();
+    static bool DeleteFriend();
+    static bool GetFriends();
+
+    static bool IsConnected();
+    static bool StartCall();
+    static bool EndCall();
 };
 #endif //BABEL_USER_HPP
