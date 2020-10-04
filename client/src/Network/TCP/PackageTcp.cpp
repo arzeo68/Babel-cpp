5a70c5e81ba293e035504f1c999f05ef52d3215f
alexis WALTER <alexis.walter@epitech.eu>
2020-10-04 19:21:20 +0200

    feat(client): update network and package structure

---
+++ client/src/Network/TCP/PackageTcp.cpp
@@ -0,0 +1,14 @@
+//
+// Created by alexis on 04/10/2020.
+//
+
+
+#include "PackageTcp.hpp"
+
+const std::string &PackageTcp::toString(std::string &send)
+{
+}
+
+std::string &PackageTcp::toPackage(std::string &string)
+{
+}
