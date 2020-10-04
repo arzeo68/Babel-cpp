56f6e05b6c8b70e6e3fa6893f6a5b60fb96044aa
alexis WALTER <alexis.walter@epitech.eu>
2020-10-04 17:04:06 +0200

    feat(feat/udp): creation of the udp voice package

--- client/main.cpp
+++ client/main.cpp
@@ -1,14 +1,4 @@
-#include <client/src/Audio/portAudio/PortAudio.hpp>
-#include <client/src/Audio/Opus/Opus.hpp>
-
 int main(int argc, char **argv)
 {
-    PortAudio p;
-    Opus o;
-    p.startRecording();
-    p.startPlaying();
-    while (1)
-    {
-        p.addSoundToQueue(p.getNextSound());
-    }
 }
+
