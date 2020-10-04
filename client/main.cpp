69b6b4f8291cc459354e60936aec6a2a95d784a0
alexis WALTER <alexis.walter@epitech.eu>
2020-10-04 13:22:13 +0200

    feat(feat/portAudio) resolving github issue

--- client/main.cpp
+++ client/main.cpp
@@ -1,17 +1,4 @@
-#include <iostream>
-
-#include "src/Audio/portAudio/PortAudio.hpp"
-#include "src/Audio/Opus/Opus.hpp"
-
-
 int main(int argc, char **argv)
 {
-    PortAudio p;
-    p.startRecording();
-    p.startPlaying();
-    while (1)
-    {
-        p.addSoundToQueue(p.getNextSound());
-    }
 }
 
