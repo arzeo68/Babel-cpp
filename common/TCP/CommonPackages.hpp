a37ea8cd54d9e0211ee83f52738c6ab18075180c
Jean-Gaël <jean-gael.choppe@epitech.eu>
2020-10-04 20:43:45 +0200

    feat(common-packages): add tcp packages struct

--- common/TCP/CommonPackages.hpp
+++ common/TCP/CommonPackages.hpp
@@ -1,28 +1,66 @@
-//
-// Created by whosdad on 04/10/2020.
-//
+/*
+** EPITECH PROJECT, 2020
+** babel
+** File description:
+** TODO: CHANGE DESCRIPTION.
+*/
 
-#ifndef BABEL_COMMONPACKAGES_HPP
-#define BABEL_COMMONPACKAGES_HPP
 
-#pragma pack(push, 1)
+#ifndef SRC_COMMON_PROTOCOL_
+#define SRC_COMMON_PROTOCOL_
 
 #include <cstdint>
+#include <string>
+#include <vector>
 
-struct PackageServer {
-    uint16_t magic;
-    uint16_t id;
-    uint8_t method;
-    uint8_t command;
-    char args[256];
-};
-#pragma pack(pop)
+namespace Common {
+    static const constexpr uint16_t MagicNumber = 0xFABA;
 
-#pragma pack(push, 1)
-struct ResponseServer {
-    uint8_t code;
-    char res[256];
-};
-#pragma pack(pop)
+    enum class HTTPCodes_e : uint16_t {
+        OK = 200,
+        BAD_REQUEST = 400,
+        UNAUTHORIZED = 401,
+        FORBIDDEN = 403,
+        NOT_FOUND = 404,
+        METHOD_NOT_ALLOWED = 405,
+        INTERNAL_SERVER_ERROR = 500,
+    };
 
-#endif //BABEL_COMMONPACKAGES_HPP
+    #pragma pack(push, 1)
+    struct Response {
+        HTTPCodes_e code;
+        char msg[256];
+    };
+    #pragma pack(pop)
+
+    static const Response InvalidMethodTemplate = {
+            .code = HTTPCodes_e::METHOD_NOT_ALLOWED,
+            .msg = "Method not allowed",
+    };
+
+    static const Response BadRequestTemplate = {
+            .code = HTTPCodes_e::BAD_REQUEST,
+            .msg = "Missing argument",
+    };
+
+    static const constexpr uint8_t MAX_ROUTE_ID = 2;
+    typedef uint8_t HTTPMethod_t;
+    enum Method : HTTPMethod_t {
+        GET,
+        DELETE,
+        POST,
+        PUT
+    };
+
+    #pragma pack(push, 1)
+    struct PackageServer {
+        uint16_t magic;
+        uint16_t id;
+        Method method;
+        uint8_t command;
+        char args[256];
+    };
+    #pragma pack(pop)
+}
+
+#endif
