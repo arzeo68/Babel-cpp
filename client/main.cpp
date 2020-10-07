ec74dc30ae73ff903e5c23bbf143f680486339b8
Clément RUFFINONI <clement.ruffinoni@epitech.eu>
2020-10-07 15:24:24 +0200

    fix(server): windows compilation

--- client/main.cpp
+++ client/main.cpp
@@ -22,7 +22,8 @@ void readSocket(boost::asio::ip::tcp::socket *socket) {
 }
 
 void writeSocket(boost::asio::ip::tcp::socket *socket, Common::PackageServer&& p) {
-    std::cout << "Will write w/ route " + std::to_string(p.command) + "..." << std::endl;
+    std::cout << "Will write w/ route " + std::to_string(p.command) + "..."
+              << std::endl;
     std::vector<boost::asio::const_buffer> buffers;
     buffers.emplace_back(boost::asio::buffer(&p, sizeof(p)));
 
@@ -32,25 +33,44 @@ void writeSocket(boost::asio::ip::tcp::socket *socket, Common::PackageServer&& p
     readSocket(socket);
 }
 
-int main(__attribute__((unused)) int argc, char **argv)
-{
+#ifdef _WIN32
+    #define UNUSED(v)
+#else
+    #define UNUSED(v) __attribute__((unused)) v
+#endif
+
+int main(int UNUSED(argc), char **argv) {
     boost::asio::ip::tcp::endpoint ep(
-        boost::asio::ip::address::from_string("192.168.1.11"), 4242);
+        boost::asio::ip::address::from_string("10.14.58.193"), 4242);
     boost::asio::io_service service;
+
+    /*
+     * 0: USER EXISTS
+     * 1: LOGIN
+     * 2: REGISTER
+     * 3: UPDATE STATUS
+     */
 
     if (argv[0] == nullptr)
         return (1);
     auto sock = new boost::asio::ip::tcp::socket(service);
     std::cout << "Connecting to an ip" << std::endl;
     sock->connect(ep);
-    writeSocket(sock, Common::PackageServer {
+    //writeSocket(sock, Common::PackageServer {
+    //    Common::g_MagicNumber,
+    //    0,
+    //    Common::HTTP_POST,
+    //    2,
+    //    "admin|admin"
+    //});
+    writeSocket(sock, Common::PackageServer{
         Common::g_MagicNumber,
         0,
         Common::HTTP_POST,
         1,
         "admin|admin"
     });
-    writeSocket(sock, Common::PackageServer {
+    writeSocket(sock, Common::PackageServer{
         Common::g_MagicNumber,
         0,
         Common::HTTP_POST,
