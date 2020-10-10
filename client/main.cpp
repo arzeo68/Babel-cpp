ab1dd9134318cde9cc2b0b232d44b98d8beb6ab3
alexis <alexis.walter@epitech.eu>
2020-10-10 14:10:36 +0200

    feat(feat/udp): fix review

--- client/main.cpp
+++ client/main.cpp
@@ -1,14 +1,3 @@
-//#include <zconf.h>
-//#include <QtCore/QCoreApplication>
-//#include "src/Network/UDP/NetworkUdp.hpp"
-
-
-// test main
 int main(int argc, char **argv)
 {
-//    QCoreApplication a(argc, argv);
-//    NetworkUDP p;
-//    p.startConnection(argv[1], argv[2]);
-//    return a.exec();
 }
-
