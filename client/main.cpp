760e716b612b588e5a9c50886b1062144bc1c600
Jean-Gaël <jean-gael.choppe@epitech.eu>
2020-10-12 01:27:30 +0200

    fix(master): remove unused imports

--- client/main.cpp
+++ client/main.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-#include "src/Audio/portAudio/PortAudio.hpp"
-#include "src/Audio/Opus/Opus.hpp"
-#include <QApplication>
-#include <QPushButton>
-#include <QtWidgets/QMainWindow>
-#include <QtWidgets/QDesktopWidget>
-#include <client/src/GUI/MainWindow.hpp>
-#include <QtWidgets/QGraphicsScene>
-#include <QtWidgets/QGraphicsView>
-#include <QtWidgets/QStackedWidget>
-#include <QtWidgets/QVBoxLayout>
-#include <QtWidgets/QButtonGroup>
-#include <zconf.h>
-#include <QtCore/QTimer>
-#include <client/src/GUI/GUIController/GUIController.hpp>
+#include <QtWidgets/QApplication>
+#include "client/src/GUI/GUIController/GUIController.hpp"
 
 
 int main(int argc, char **argv)
